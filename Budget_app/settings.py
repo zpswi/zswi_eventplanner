6b9471c007e309e8f0d9d937ef9770ed3cac4252
Jiří Heralt <info@jiriheralt.cz>
2024-10-08 08:34:27 +0200

    first view

--- Budget_app/settings.py
+++ Budget_app/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = "Budget_app.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / "app/templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
