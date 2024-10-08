f8833c6c0544d6e85cd15d30cbb6f20186b11519
Jan Bednář <honzaxp01@gmail.com>
2024-10-08 08:21:36 +0200

    Add base models

--- app/models.py
+++ app/models.py
@@ -1,8 +1,14 @@
+from django.contrib.auth.models import User as AuthUser
 from django.db import models
+
+class User(AuthUser):
+    pass
 
 # Create your models here.
 class Event(models.Model):
     title = models.CharField(max_length=200)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    participants = models.ManyToManyField(User)
     description = models.TextField(blank=True, null=True)
     location = models.CharField(max_length=255, blank=True, null=True)
     start_time = models.DateTimeField()
