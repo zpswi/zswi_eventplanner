f0f212ffc09d39cf9e74285553b7f4f11a97b694
Jiří Heralt <info@jiriheralt.cz>
2024-10-22 08:43:01 +0200

    user login, user logout

--- app/urls.py
+++ app/urls.py
@@ -3,11 +3,10 @@ from django.urls import path
 from . import views
 
 
-
 urlpatterns = [
     path("", views.index, name="index"),
     path("/login", views.login_test, name="login"),
-    path("/logout", views.logout, name="logout"),
+    path("/logout", views.user_logout, name="user_logout"),
     path("/events", views.events, name="events"),
     path("/event/<int:event_id>", views.event_detail, name="event_detail"),
     path("/add_event", views.add_event, name="add_event"),
