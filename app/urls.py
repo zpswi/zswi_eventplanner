fa4c26212b6f2b91e0103fb1018eff250ce7fa6b
Jiří Heralt <info@jiriheralt.cz>
2024-10-22 08:27:47 +0200

    move urls from /app prefix

--- app/urls.py
+++ app/urls.py
@@ -7,6 +7,7 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("/login", views.login_test, name="login"),
+    path("/logout", views.logout, name="logout"),
     path("/events", views.events, name="events"),
     path("/event/<int:event_id>", views.event_detail, name="event_detail"),
     path("/add_event", views.add_event, name="add_event"),
