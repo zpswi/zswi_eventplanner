ffb6780309d46d55b035ffe69d58003bbb73d33f
Roman Slanina <roman.slanin@gmail.com>
2024-11-04 08:50:54 +0100

    sign to event and event filter added

--- app/views.py
+++ app/views.py
@@ -6,9 +6,11 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from app.forms import AddEventForm
 from django.contrib.auth import login, authenticate
-from django.shortcuts import  redirect
+from django.shortcuts import redirect
 from app.models import Event
 from django.contrib.auth import logout
+from .forms import EventFilterForm
+
 
 def index(request):
     html = render(request, "app/index.html")
@@ -20,9 +22,8 @@ def login_test(request):
     return render(request, "app/index.html")
 
 
-
 def add_event(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         form = AddEventForm(request.POST)
         form.instance.user = request.user
 
@@ -30,29 +31,69 @@ def add_event(request):
             form.save()
             messages.add_message(request, messages.SUCCESS, "Created new event")
 
-    return HttpResponseRedirect(reverse('index'))
+    return HttpResponseRedirect(reverse("index"))
+
 
 def events(request):
-    return render(request, "events/list.html", {"events": Event.objects.all()})
-    
+    form = EventFilterForm(request.GET)
+    events = Event.objects.all()
+
+    if form.is_valid():
+        if form.cleaned_data["title"]:
+            events = events.filter(title__icontains=form.cleaned_data["title"])
+        if form.cleaned_data["location"]:
+            events = events.filter(location__icontains=form.cleaned_data["location"])
+        if form.cleaned_data["start_time_after"]:
+            events = events.filter(
+                start_time__gte=form.cleaned_data["start_time_after"]
+            )
+        if form.cleaned_data["start_time_before"]:
+            events = events.filter(
+                start_time__lte=form.cleaned_data["start_time_before"]
+            )
+        if form.cleaned_data["end_time_after"]:
+            events = events.filter(end_time__gte=form.cleaned_data["end_time_after"])
+        if form.cleaned_data["end_time_before"]:
+            events = events.filter(end_time__lte=form.cleaned_data["end_time_before"])
+        if form.cleaned_data["all_day"] is not None:
+            events = events.filter(all_day=form.cleaned_data["all_day"])
+
+    return render(request, "events/list.html", {"events": events, "form": form})
+
+
 def event_detail(request, event_id):
     event = Event.objects.get(id=event_id)
     return render(request, "events/detail.html", {"event": event})
+
 
 def user_logout(request):
     logout(request)
     return render(request, "user/logout.html")
 
+
 def signup(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         form = UserCreationForm(request.POST)
         if form.is_valid():
             form.save()
-            username = form.cleaned_data.get('username')
-            raw_password = form.cleaned_data.get('password1')
+            username = form.cleaned_data.get("username")
+            raw_password = form.cleaned_data.get("password1")
             user = authenticate(username=username, password=raw_password)
             login(request, user)
-            return redirect('index')
+            return redirect("index")
     else:
         form = UserCreationForm()
-    return render(request, 'registration/signup.html', {'form': form})
+    return render(request, "registration/signup.html", {"form": form})
+
+
+def sign_to_event(request, event_id):
+    event = Event.objects.get(id=event_id)
+    if event.participants.count() >= event.capacity:
+        messages.add_message(request, messages.ERROR, "Event is full")
+        return HttpResponseRedirect(reverse("event_detail", args=[event_id]))
+    if request.user in event.participants.all():
+        messages.add_message(request, messages.ERROR, "You are already signed up")
+        return HttpResponseRedirect(reverse("event_detail", args=[event_id]))
+    event.participants.add(request.user)
+    messages.add_message(request, messages.SUCCESS, "Signed up")
+    return HttpResponseRedirect(reverse("event_detail", args=[event_id]))
